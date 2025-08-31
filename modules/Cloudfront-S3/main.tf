8cacccffd20dcaa344cf8f896be5fea0b4513389
jpsankari <jpsankari@hotmail.com>
2025-08-31 09:52:29 +0800

    Updated Cer ARN

--- modules/Cloudfront-S3/main.tf
+++ modules/Cloudfront-S3/main.tf
@@ -58,7 +58,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   viewer_certificate {
-   acm_certificate_arn            = var.acm_certificate_arn  # Valid ACM cert ARN in us-east-1
+   #acm_certificate_arn            = var.acm_certificate_arn  # Valid ACM cert ARN in us-east-1
+  acm_certificate_arn            = var.acm_certificate_arn[var.env]
   ssl_support_method             = "sni-only"
   minimum_protocol_version       = "TLSv1.2_2021"
   cloudfront_default_certificate = false
