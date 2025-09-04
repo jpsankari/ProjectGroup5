3e02a6fd0452815caae0c63d30fe8b4dac497419
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-09-04 13:57:12 +0800

    Update main.tf

--- modules/Lambda-DB-API/main.tf
+++ modules/Lambda-DB-API/main.tf
@@ -126,7 +126,8 @@ resource "aws_api_gateway_method" "orders_post" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.orders_resource.id
   http_method   = "POST"
-  authorization = "NONE"
+  #authorization = "NONE"
+  #authorization = "AWS_IAM""
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
