08dc0b5901eaa9bda269da3d24cd654f9ea618c6
jpsankari <jpsankari@hotmail.com>
2025-09-05 12:04:57 +0800

    updated cloud front

--- modules/Cloudfront-S3/main.tf
+++ modules/Cloudfront-S3/main.tf
@@ -112,7 +112,7 @@ resource "aws_cloudwatch_log_group" "central_log_group" {
 
 resource "aws_wafv2_web_acl" "oneclickbouquet_cloudfront_waf" {
   provider = aws.virginia
-  name  = "oneclickbouquet_cloudfront-waf-{var.env}"
+  name  = "oneclickbouquet_cloudfront-waf-${var.env}"
   scope = "CLOUDFRONT"
 
   default_action {
