0a451609ff3ae567de18a0fe84e843c662170210
jpsankari <jpsankari@hotmail.com>
2025-09-05 12:09:25 +0800

    updated cloud front

--- modules/Cloudfront-S3/main.tf
+++ modules/Cloudfront-S3/main.tf
@@ -98,10 +98,6 @@ resource "aws_cloudwatch_log_group" "central_log_group" {
   provider          = aws.virginia
   name              = "/aws/oneclickbouquet/${aws_wafv2_web_acl.oneclickbouquet_cloudfront_waf.name}"
   retention_in_days = 1
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 
