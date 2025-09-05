c09bcd8c4850a1123e1626abadf12f1365d7cb6c
jpsankari <jpsankari@hotmail.com>
2025-09-05 10:12:08 +0800

    commended Snyk

--- modules/Cloudfront-S3/main.tf
+++ modules/Cloudfront-S3/main.tf
@@ -98,9 +98,9 @@ resource "aws_cloudwatch_log_group" "central_log_group" {
   name              = "/aws/oneclickbouquet/${aws_wafv2_web_acl.oneclickbouquet_cloudfront_waf.name}"
   retention_in_days = 1
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  #lifecycle {
+   # prevent_destroy = true
+  #}
 }
 
 #==================================================
