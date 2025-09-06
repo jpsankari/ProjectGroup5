53b246b99933d4ff522c129f017a7072c964c84f
jpsankari <jpsankari@hotmail.com>
2025-09-06 22:25:46 +0800

    Updated Cloudwatch

--- modules/Lambda-DB-API/main.tf
+++ modules/Lambda-DB-API/main.tf
@@ -6,12 +6,14 @@ module "Lambda-DB_log_group" {
   name             = "/aws/oneclickbouquet/my-api-logs-${var.env}"
   retention_in_days = 1
   log_stream_name    = "myapp-log-stream"
+  /*
   metric_filter_name = "ErrorCountFilter"
   metric_filter_pattern = "{ $.level = \"ERROR\" }"
   metric_name        = "ErrorCount"
   metric_namespace   = "MyApp/Metrics"
   metric_value       = "1"
   metric_unit        = "Count"
+  */
   tags = {
     Environment = "${var.env}"
     Project     = "oneclickbouquet-MyAPI"
