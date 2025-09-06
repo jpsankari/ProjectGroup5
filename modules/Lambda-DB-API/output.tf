e0426c9988cca5afb56c58b51d6411bb1611f334
jpsankari <jpsankari@hotmail.com>
2025-09-06 12:57:49 +0800

    Added destroy

--- modules/Lambda-DB-API/output.tf
+++ modules/Lambda-DB-API/output.tf
@@ -6,3 +6,6 @@ output "api_endpoint" {
 value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
 }
 
+output "api_invoke_url" {
+  value = "https://${aws_api_gateway_rest_api.example.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_deployment.example.stage_name}"
+}
