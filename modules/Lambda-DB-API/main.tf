b8f5789fb3f42aa9e7099eed18a75b146169efc7
jpsankari <jpsankari@hotmail.com>
2025-09-06 18:50:11 +0800

    Updated API Integration Line 265

--- modules/Lambda-DB-API/main.tf
+++ modules/Lambda-DB-API/main.tf
@@ -262,6 +262,7 @@ resource "aws_lambda_permission" "api_gateway_permission" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.process_order.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/**"
+  #source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/**"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/${aws_api_gateway_stage.api_stage.stage_name}/POST/orders"
 }
 
