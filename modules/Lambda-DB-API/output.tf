61df2945c0747f1842ed57b4897054ee5b432b76
jpsankari <jpsankari@hotmail.com>
2025-09-06 13:47:22 +0800

    updated output

--- modules/Lambda-DB-API/output.tf
+++ modules/Lambda-DB-API/output.tf
@@ -5,7 +5,3 @@ output "post_integration_id" {
 output "api_endpoint" {
 value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
 }
-
-output "api_invoke_url" {
-  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.api_stage.stage_name}"
-}
