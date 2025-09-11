dbdbd4abb6b32b1023c99b02ea8ee4c89f817976
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-09-11 17:44:04 +0800

    Update main.tf

--- modules/CloudWatch/main.tf
+++ modules/CloudWatch/main.tf
@@ -10,3 +10,17 @@ resource "aws_cloudwatch_log_stream" "this" {
   log_group_name = aws_cloudwatch_log_group.this.name
 }
 
+resource "aws_cloudwatch_log_metric_filter" "this" {
+  name           = var.metric_filter_name
+  log_group_name = aws_cloudwatch_log_group.this.name
+  pattern        = var.metric_filter_pattern
+
+  metric_transformation {
+    name      = var.metric_name
+    namespace = var.metric_namespace
+    value     = var.metric_value
+    unit      = var.metric_unit
+  }
+}
+
+
