8bc17daad379ed8cbeef7a055458c962a0a820f6
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-09-11 17:48:50 +0800

    Update variables.tf

--- modules/CloudWatch/variables.tf
+++ modules/CloudWatch/variables.tf
@@ -1,63 +1,68 @@
-variable "env" {
-  description = "The environment (e.g., dev, prod) for the infrastructure"
-  type        = string
-  default     = "" # You can set the default to whatever environment you are working in
-}
-
-variable "name" {
-  description = "Name of the CloudWatch Log Group"
-  type        = string
-}
-
-variable "retention_in_days" {
-  description = "Number of days to retain the logs"
-  type        = number
-  default     = 1
-}
-
+variable "name" {}
+variable "retention_in_days" { default = 7 }
 variable "tags" {
-  description = "Tags to apply to the Log Group"
-  type        = map(string)
-  default     = {}
-}
-variable "log_stream_name" {
-  description = "Name of the CloudWatch Log Stream"
-  type        = string
-  default     = "default-log-stream"
+  type    = map(string)
+  default = {}
 }
 
+variable "log_stream_name" {
+  type    = string
+  default = null
+}
+
+# Metric Filter
 variable "metric_filter_name" {
-  description = "Name of the CloudWatch Metric Filter"
-  type        = string
-  default     = "ExampleMetricFilter"
+  type    = string
+  default = null
 }
 
 variable "metric_filter_pattern" {
-  description = "Pattern for the metric filter"
-  type        = string
-  default     = ""
+  type    = string
+  default = null
 }
 
 variable "metric_name" {
-  description = "Name of the metric to emit"
-  type        = string
-  default     = "ExampleMetric"
+  type    = string
+  default = null
 }
 
 variable "metric_namespace" {
-  description = "Namespace for the metric"
-  type        = string
-  default     = "MyAppNamespace"
+  type    = string
+  default = null
 }
 
 variable "metric_value" {
-  description = "Value to emit when the filter matches"
-  type        = string
-  default     = "1"
+  type    = string
+  default = "1"
 }
 
 variable "metric_unit" {
-  description = "Unit for the metric"
-  type        = string
-  default     = "Count"
+  type    = string
+  default = "Count"
+}
+
+# Alarm
+variable "enable_alarm" {
+  type    = bool
+  default = false
+}
+
+variable "alarm_name" {
+  type    = string
+  default = null
+}
+
+variable "alarm_threshold" {
+  type    = number
+  default = 1
+}
+
+variable "alarm_period" {
+  type    = number
+  default = 300
+}
+
+variable "alarm_actions" {
+  type    = list(string)
+  default = []
 }
