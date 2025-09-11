9fa55f8f38e8f520f80f32ba5aac3a4c01be0d62
jpsankari <JPSANKARI@HOTMAIL.COM>
2025-09-11 18:12:36 +0800

    Update output.tf

--- modules/CloudWatch/output.tf
+++ modules/CloudWatch/output.tf
@@ -1,9 +1,1 @@
-output "log_group_name" {
-  description = "Name of the CloudWatch Log Group"
-  value       = aws_cloudwatch_log_group.this.name
-}
-output "log_stream_name" {
-  description = "Name of the CloudWatch Log Stream"
-  value       = aws_cloudwatch_log_stream.this.name
-}
 
